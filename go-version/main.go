5e5d1a5d983ec3ca779c28dc46708547f3d0783e
vano <davidevanoncini2003@gmail.com>
2021-05-21 23:51:39 +0200

    fixed css in goversion, had to install new version of golang

--- go-version/main.go
+++ go-version/main.go
@@ -1,33 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	// "fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-var LoginPage string
-
-func init() {
-	data, err := ioutil.ReadFile("./pages/login.html")
-	if err != nil {
-		panic(err)
-	}
-	LoginPage = string(data)
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
+func loginHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content Type", "text/html")
-	fmt.Fprintf(w, LoginPage)
+	content, err := os.ReadFile("pages/login.html")
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	w.Write(content)
 }
 
 func main() {
 	// baseLink := http.FileServer(http.Dir("./pages/login"))
-	http.HandleFunc("/", login)
+	http.HandleFunc("/", loginHandler)
 	// http.HandleFunc("/api/user/signup", handleSingUp)
 
-	fmt.Println("inizio server sulla porta 8080")
+	log.Println("inizio server sulla porta 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}

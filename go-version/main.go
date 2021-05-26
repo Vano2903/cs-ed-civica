a9c4ee564339c65b952384d3f95d8c9977650352
Vanoncini Davide 18008 <vanoncini.18008@PALEOCAPA.DID>
2021-05-26 14:48:45 +0200

    added check login post acion on login endpoint in go-version

--- go-version/main.go
+++ go-version/main.go
@@ -44,8 +44,6 @@ type Voter struct {
 	Token    string
 	Name     string
 	LastName string
-	Email    string
-	Password string
 	Voted    bool
 	Vote     int
 	Position int
@@ -53,14 +51,17 @@ type Voter struct {
 
 type Server struct {
 	Voters        []Voter
-	UsersForLogin []string
+	UsersForLogin map[string]string
+	UsersFromFile []string
 }
 
 func (s *Server) init() error {
+	s.UsersForLogin = make(map[string]string)
 	data := ReadFile("config/senatore.txt")
 	// datas := File.Read(data)
 	datas := strings.Split(string(data), "\n")
 	for pos, data := range datas {
+		s.UsersFromFile = append(s.UsersFromFile, data)
 		var t Token
 		elements := strings.Split(data, ";")
 		err := t.GenToken(pos + 1)
@@ -68,23 +69,53 @@ func (s *Server) init() error {
 			return err
 		}
 		toAdd := elements[3] + ";" + elements[4] + ";" + t.Token
-		s.UsersForLogin = append(s.UsersForLogin, toAdd)
+		s.UsersForLogin[t.Token] = toAdd
 	}
 	return nil
 }
 
 //start the server and listen on localhost:8080
 func (s Server) Start() error {
-
 	http.HandleFunc("/", s.loginHandler)
 	return http.ListenAndServe(":8080", nil)
 }
 
+//print all logins codes, for now it's kinda just for debug
 func (s Server) PrintLogins() {
 	fmt.Println(s.init())
 	for _, a := range s.UsersForLogin {
 		fmt.Println(a)
 	}
+}
+
+//check if the login is correct, if so then add it to Voters
+func (s *Server) CheckLoginAndAdd(login string) bool {
+	//split the login string
+	elements := strings.Split(login, ";")
+	correctLogin, exist := s.UsersForLogin[elements[2]]
+	//if given token exist
+	if exist {
+		//check if login is the same as loaded from file
+		if correctLogin == login {
+			var toAdd Voter
+			var fullVoterElements []string
+			var i int
+			for _, check := range s.UsersForLogin {
+				if check == login {
+					fullVoterElements = strings.Split(s.UsersFromFile[i], ";")
+				}
+				i++
+			}
+			//create Voter object
+			id, _ := strconv.Atoi(fullVoterElements[0])
+			toAdd.Id = id
+			toAdd.Token = elements[2]
+			toAdd.Name = fullVoterElements[1]
+			toAdd.LastName = fullVoterElements[2]
+			s.Voters = append(s.Voters, toAdd)
+		}
+	}
+	return exist
 }
 
 //handle the clients that connect to "/" and respond with the login page
@@ -104,11 +135,14 @@ func (s Server) loginHandler(w http.ResponseWriter, r *http.Request) {
 	case "POST":
 		reqBody, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			log.Fatal(err)
+			log.Println("errore nella post a login handler", err)
 		}
-		fmt.Printf("%s\n", reqBody)
-		w.Write([]byte("Received a POST request\n"))
-
+		log.Printf("%s\n", reqBody)
+		if s.CheckLoginAndAdd(string(reqBody)) {
+			w.Write([]byte("Correct Login\n"))
+		} else {
+			w.Write([]byte("Nope ;-; xD\n"))
+		}
 	default:
 		w.WriteHeader(http.StatusNotImplemented)
 		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
@@ -117,7 +151,6 @@ func (s Server) loginHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var s Server
-
 	log.Println("inizio server sulla porta 8080")
 	s.PrintLogins()
 	s.init()
